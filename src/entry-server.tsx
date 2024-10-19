7ccecbaa6ecbf668a2d305def51bcef4dafaf738
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-19 11:40:43 +0200

    chore: add favicon and meta

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,9 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="alternate icon" href="/favicon.ico" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
+          <meta name="theme-color" content="#08162B"></meta>
           {assets}
         </head>
         <body class="font-sans">
